353520546b93a80770db5fed2284d6cea85c57bb
teukumunawark <munawartmk@gmail.com>
2022-12-18 21:10:31 +0700

    create Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,46 @@
-import logo from '../assets/logo.png';
-import { navLinks } from '../constants';
-import Botton from './Botton';
+import 'flowbite';
+import Botton from '../components/Botton';
+import Logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    // <div className="py-6">
-    //   <div className="bg-primary flex justify-between items-center">
-    //     <div>
-    //       <img src={logo} alt="logo" />
-    //     </div>
 
-    //     <div className="flex items-center space-x-6">
-    //       <ul className="flex ml-auto space-x-4 text-gray-600">
-    //         <li className="text-base font-poppins font-medium cursor-pointer">Our Services</li>
-    //         <li className="text-base font-poppins font-medium cursor-pointer">Why Us</li>
-    //         <li className="text-base font-poppins font-medium cursor-pointer">Testimonial</li>
-    //         <li className="text-base font-poppins font-medium cursor-pointer">FAQ</li>
-    //       </ul>
-    //       <Botton />
-    //     </div>
+    <div className="xl:max-w-[1290px] w-full container">
+      <nav className="bg-primary border-gray-200 py-2.5 md:py-1 rounded dark:bg-gray-900">
+        <div className="flex flex-wrap items-center justify-between mx-auto sm:py-1 md:py-0 sm:px-10 md:px-0">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <img src={Logo} className="h-6 mr-3 sm:h-9" alt="Logo" />
+          </a>
+          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
 
-    //   </div>
-    // </div>
-    <div className="navbar ">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-              </a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="flex flex-col md:items-center p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-medium md:border-0 md:bg-primary dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-white bg-green-500 rounded md:bg-transparent md:text-green-500 md:p-0 dark:text-white" aria-current="page">Our Services</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Why Us</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Testimonial</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">FAQ</a>
+              </li>
+              <li>
+                <Botton />
+              </li>
+            </ul>
+          </div>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-
-
-      <div className="navbar-end">
-        <div className="navbar-center hidden lg:flex ">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li><a>Item 3</a></li>
-            <li><a>Item 3</a></li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-        <button className="btn btn-success text-white ">Register</button>
-      </div>
+      </nav>
     </div>
+
+
+
   )
 }
 
