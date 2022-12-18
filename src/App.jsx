58e5a046503695b51eb24666e62fc79986f43693
teukumunawark <munawartmk@gmail.com>
2022-12-18 21:09:17 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import style from './style';
 import { Navbar, Hero, Footer } from './components';
-import Form from './components/Form';
 
 function App() {
   return (
     <div className="bg-primary">
-      <div className={`bg-primary`}>
-          <Navbar />
-      </div>
-
-      <div className={`bg-primary`}>
-        <Hero />
-      </div>
-
-      <div className={`bg-white`}>
-        <Form/>
-      </div>
-
+      <Navbar />
+      <Hero />
       <div className={`bg-white`}>
         <Footer />
       </div>
