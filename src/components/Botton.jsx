5e0ab71c565ab55a14c1725fd55706a1f247074b
teukumunawark <munawartmk@gmail.com>
2022-12-20 17:31:08 +0700

    update button component

--- src/components/Botton.jsx
+++ src/components/Botton.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Botton = ({ href }) => {
+
+const Botton = ({ href, title }) => {
   return (
-    <button
-      href={href}
-      type="button"
-      className="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-8 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      Register
-    </button>
+    <Link
+      className=
+      'hover:bg-green-500 bg-[#5CB85F] text-white rounded-sm px-8 py-2.5 font-poppins text-sm transition text-center'
+      to={href}
+    >{title}</Link>
   )
 }
 
