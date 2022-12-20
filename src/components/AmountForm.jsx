054d42ab432927d93d919aca2c8de5c79cfb56ab
teukumunawark <munawartmk@gmail.com>
2022-12-20 17:32:57 +0700

    update component

---
+++ src/components/AmountForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AmountForm = () => {
+  return (
+    <div>
+      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jumlah Penumpang (optional)</label>
+      <input 
+      type="text" 
+      id="tipe-driver" 
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-green-500 focus:border-green-500 block w-full lg:min-w-[230px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+      placeholder="Jumlah Penumpang"
+      >
+      </input>
+    </div>
+  )
+}
+
+export default AmountForm
