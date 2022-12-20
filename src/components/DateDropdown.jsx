d64561160aeede520016f379fde6bb9eed439114
teukumunawark <munawartmk@gmail.com>
2022-12-20 17:42:30 +0700

    update

--- src/components/DateDropdown.jsx
+++ src/components/DateDropdown.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const DateDropdown = () => {
   return (
     <div>
-      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal</label>
-      <select id="tipe-driver" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-green-500 focus:border-green-500 block w-full lg:min-w-[230px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+      <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal</label>
+      <select id="tanggal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-green-500 focus:border-green-500 block w-full lg:min-w-[230px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
         <option selected>Pilih Tanggal</option>
         <option value="Dengan Driver">United States</option>
         <option value="Tanpa Driver">Canada</option>
