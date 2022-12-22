821c9976f4e9381a6a46d83d98eda16144ebba25
teukumunawark <munawartmk@gmail.com>
2022-12-22 11:59:44 +0700

    update card and botton

--- src/components/Botton.jsx
+++ src/components/Botton.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-const Botton = ({ href, title }) => {
+const Botton = ({ onClick, title }) => {
   return (
-    <Link
-      className=
-      'hover:bg-green-500 bg-[#5CB85F] text-white rounded-sm px-8 py-2.5 font-poppins text-sm transition text-center'
-      to={href}
-    >{title}</Link>
+    <button
+      onClick={onClick}
+      type="button"
+      className='hover:bg-green-500 bg-[#5CB85F] text-white rounded-sm px-8 py-2.5 font-poppins text-sm transition text-center'>
+      {title}
+    </button>
   )
 }
 
