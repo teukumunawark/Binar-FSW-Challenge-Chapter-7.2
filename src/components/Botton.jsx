53ce61fb8bfbdaab2c2c22645c888a1b71747848
teukumunawark <munawartmk@gmail.com>
2022-12-23 16:03:49 +0700

    update

--- src/components/Botton.jsx
+++ src/components/Botton.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-const Botton = ({ onClick, title }) => {
+const Botton = ({ type, title }) => {
   return (
     <button
-      onClick={onClick}
-      type="button"
+      type={type}
       className='hover:bg-green-500 bg-[#5CB85F] text-white rounded-sm px-8 py-2.5 font-poppins text-sm transition text-center'>
       {title}
     </button>
